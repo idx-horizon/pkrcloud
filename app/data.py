3eda3a9748b821290c0e58049ca51fb42365c657
idx <idx-horizon@users.noreply.github.com>
2024-07-18 22:29:17 +0100

    wip

--- app/data.py
+++ app/data.py
@@ -3,15 +3,14 @@ import json
 def getdata(id):
     with open(f'{id}.pkr','r',encoding='utf-8') as f:
         data = json.loads(f.read())
-        event_count = len(set([x['Event'] for x in data[1]['runs']]))
-        return data[1]['runs'], data[1]['title'], event_count
+        d = data[1] # [1] = the runs, [0] = countries
+        event_count = len(set([x['Event'] for x in d['runs']]))
+        title = d['title']
+#        return data[1]['runs'], data[1]['title'], event_count
 
-    c = Counter([x['Event'] for x in data])
+    c = Counter([x['Event'] for x in d])
 
     c[f'Events: {event_count}'] =  10
-
-#    yrc = Counter()
-#    yrc.update([x['Run Date'][-4:] for x in data])
 
     for yr in [x['Run Date'][-4:] for x in data]:
         c[yr]=9
@@ -20,11 +19,11 @@ def getdata(id):
     runner_name = f"{title[:title.index(' ')]}"
     runner_num_runs = f'Runs:{len(data)}'
     
-    c[runner_name]     = min(11, len(data))
-    c[runner_id]       = min(12, len(data))
-    c[runner_num_runs] = min(12, len(data))
+    c[runner_name]     = min(11, len(d))
+    c[runner_id]       = min(12, len(d))
+    c[runner_num_runs] = min(12, len(d))
 
-    pbsecs = min([convert_to_seconds(x['Time']) for x in data])
+    pbsecs = min([convert_to_seconds(x['Time']) for x in d])
     pb = 'PB: ' + str(datetime.timedelta(seconds=pbsecs))[-5:]
     c[pb] = min(12, len(data))
 
--- plotcloud.py
+++ plotcloud.py
@@ -61,29 +61,8 @@ def rank_simple(lst):
 
 def produce(id, destination, maskfile):
 
-    data, title, event_count = app.data.getdata(id)
-    c = Counter([x['Event'] for x in data])
-
-    c[f'Events: {event_count}'] =  10
-
-    yrc = Counter()
-    #c.update([x['Run Date'][-4:] for x in data])
-    for yr in [x['Run Date'][-4:] for x in data]:
-        c[yr]=9
-    yrc.update([x['Run Date'][-4:] for x in data])
-    #print(yrc)
-    #print(rank_simple(yrc))
-
-    runner_id = f"A{id}"
-    runner_name = f"{title[:title.index(' ')]}"
-    c[runner_name] = min(11, len(data))
-    c[runner_id] = min(12, len(data))
-    c[f'Runs:{len(data)}'] = min(12, len(data))
-
-    pbsecs = min([convert_to_seconds(x['Time']) for x in data])
-    pb = 'PB: ' + str(datetime.timedelta(seconds=pbsecs))[-5:]
-    c[pb] = min(12, len(data))
-
+    c = app.data.getdata(id)
+ 
     print(runner_id, runner_name, pb)
 
     mask = np.array(Image.open(maskfile))
@@ -110,11 +89,11 @@ def produce(id, destination, maskfile):
     plt.figure(figsize = (20, 12), facecolor = None)
     plt.imshow(bordered_image)
     plt.axis("off")
-    plt.title(f'{runner_id} - {runner_name} Runs: {len(data)} {pb} Events: {event_count}',
-            #y=0.5, 
-            loc='right',backgroundcolor='darkgreen',
-            rotation=0,color='white',
-            fontweight='bold',fontfamily='serif',fontsize='x-large')
+#    plt.title(f'{runner_id} - {runner_name} Runs: {len(data)} {pb} Events: {event_count}',
+#            #y=0.5, 
+#            loc='right',backgroundcolor='darkgreen',
+#            rotation=0,color='white',
+#            fontweight='bold',fontfamily='serif',fontsize='x-large')
 
     plt.savefig(f'{destination}/{id}.png')
 
