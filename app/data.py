f4177cc89bfdce0134d8ebf6f5c9fc3521251ec6
idx <idx-horizon@users.noreply.github.com>
2024-07-18 22:38:25 +0100

    wip

--- app/data.py
+++ app/data.py
@@ -11,21 +11,21 @@ def convert_to_seconds(tm):
 def getdata(id):
     with open(f'{id}.pkr','r',encoding='utf-8') as f:
         data = json.loads(f.read())
-        d = data[1] # [1] = the runs, [0] = countries
-        event_count = len(set([x['Event'] for x in d['runs']]))
-        title = d['title']
+        d = data[1]['runs'] # [1] = the runs, [0] = countries
+        event_count = len(set([x['Event'] for x in d]))
+        title = data['title']
 #        return data[1]['runs'], data[1]['title'], event_count
 
     c = Counter([x['Event'] for x in d])
 
     c[f'Events: {event_count}'] =  10
 
-    for yr in [x['Run Date'][-4:] for x in data]:
+    for yr in [x['Run Date'][-4:] for x in d]:
         c[yr]=9
 
     runner_id = f"A{id}"
     runner_name = f"{title[:title.index(' ')]}"
-    runner_num_runs = f'Runs:{len(data)}'
+    runner_num_runs = f'Runs:{len(d)}'
     
     c[runner_name]     = min(11, len(d))
     c[runner_id]       = min(12, len(d))
@@ -33,6 +33,6 @@ def getdata(id):
 
     pbsecs = min([convert_to_seconds(x['Time']) for x in d])
     pb = 'PB: ' + str(datetime.timedelta(seconds=pbsecs))[-5:]
-    c[pb] = min(12, len(data))
+    c[pb] = min(12, len(d))
 
     return c    
